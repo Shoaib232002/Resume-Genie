ef38b02d6d8611dc79ea6b2a7031a53d32019e0b
Shoaib232002 <shoaib232002@gmail.com>
2025-04-03 06:38:03 +0530

    Spell mistake

--- Backend/controller/ai.controller.js
+++ Backend/controller/ai.controller.js
@@ -1,9 +1,9 @@
-const {generateResulte} = require('../services/ai.service')
+const {generateResult} = require('../services/ai.service')
 
 module.exports.generateAI_Result = async (req ,res) => {
     try {
         const {prompt}= req.query
-        const result = await generateResulte(prompt)
+        const result = await generateResult(prompt)
         res.status(200).json(result);
     }catch(err){
         
--- Backend/services/ai.service.js
+++ Backend/services/ai.service.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-module.exports.generateResulte = async (prompt) => {
+module.exports.generateResult = async (prompt) => {
     if(!prompt){
         return {error: "Prompt is required"};
     }
